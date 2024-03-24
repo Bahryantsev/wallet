91af70873d75b474ffee21ae75f8f5e692418f3c
bahryantsev <bahryantsev@gmail.com>
2024-03-24 17:03:19 +0300

    feat: service worker msg actions

--- background.ts
+++ background.ts
@@ -1,13 +1,30 @@
+import { store } from './src/lib/storage'
 import { VaultService } from './src/lib/vault'
 import { EMessageAction, IMessage } from './src/types/messages'
 
 const actionsMap = {
-  [EMessageAction.DECRYPT]: (valut: VaultService, msg: IMessage) => {},
-  [EMessageAction.ENCRYPT]: (valut: VaultService, msg: IMessage) => {},
+  [EMessageAction.DECRYPT]: async (
+    vault: VaultService,
+    msg: IMessage,
+    port: chrome.runtime.Port
+  ) => {
+    const decryptedData = await vault.decrypt(msg.value, msg?.pw ?? '')
+    port.postMessage({...msg, value: decryptedData})
+  },
+  [EMessageAction.ENCRYPT]: async (
+    vault: VaultService,
+    msg: IMessage,
+    port: chrome.runtime.Port
+  ) => {
+    const encryptedData = await vault.encrypt(msg.value, msg.pw ?? '')
+    store(msg.key, encryptedData)
+  },
 }
 chrome.runtime.onConnect.addListener((port) => {
   const vault = new VaultService()
-  port.onMessage.addListener((msg) => {})
+  port.onMessage.addListener((msg: IMessage) => {
+    actionsMap[msg.action](vault, msg, port)
+  })
 })
 
 chrome.alarms.create('keep-alive', {
